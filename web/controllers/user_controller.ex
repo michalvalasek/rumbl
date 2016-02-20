bb8807c3c6bf67e5c7c1b6459dda27bd7f109d90
Michal Valasek <michal.valasek@gmail.com>
2016-02-20 22:43:46 +0100

    Add authenticate function plug (which only allows logged-in users) to the UserController

--- web/controllers/user_controller.ex
+++ web/controllers/user_controller.ex
@@ -1,5 +1,8 @@
 defmodule Rumbl.UserController do
   use Rumbl.Web, :controller
+
+  plug :authenticate when action in [:index, :show]
+
   alias Rumbl.User
 
   def index(conn, _params) do
@@ -27,6 +30,19 @@ defmodule Rumbl.UserController do
       {:error, changeset} ->
         conn
         |> render("new.html", changeset: changeset)
+    end
+  end
+
+  # Private
+
+  defp authenticate(conn, _opts) do
+    if conn.assigns.current_user do
+      conn
+    else
+      conn
+      |> put_flash(:error, "You must be logged in to access that page")
+      |> redirect(to: page_path(conn, :index))
+      |> halt()
     end
   end
 end
