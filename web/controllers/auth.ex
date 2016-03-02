2c87b427215db49bc0948eff2d5ccb99f4d5c605
Michal Valasek <michal.valasek@gmail.com>
2016-03-03 00:12:38 +0100

    Add tests for Auth module plug.

--- web/controllers/auth.ex
+++ web/controllers/auth.ex
@@ -9,20 +9,21 @@ defmodule Rumbl.Auth do
     Keyword.fetch!(opts, :repo)
   end
 
+  # places User from session into conn.assigns
+  # or keeps the one that's already there
+  # or puts nil
   def call(conn, repo) do
     user_id = get_session(conn, :user_id)
     cond do
       user = conn.assigns[:current_user] ->
         conn
 
-      user = user = user_id && repo.get(Rumbl.User, user_id) ->
+      user = user_id && repo.get(Rumbl.User, user_id) ->
         assign(conn, :current_user, user)
 
       true ->
         assign(conn, :current_user, nil)
     end
-
-
   end
 
   def login(conn, user) do
