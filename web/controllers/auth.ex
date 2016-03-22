837b5b49324715c0d98cb5a3d71931aa19f51c0b
Michal Valasek <michal.valasek@gmail.com>
2016-03-22 23:24:01 +0100

    Add socket authentication (in Auth module generate token and add it to assigns if user is authenticated. Then on client use this token when establishing socket connection to server).

--- web/controllers/auth.ex
+++ web/controllers/auth.ex
@@ -14,12 +14,13 @@ defmodule Rumbl.Auth do
   # or puts nil
   def call(conn, repo) do
     user_id = get_session(conn, :user_id)
+
     cond do
       user = conn.assigns[:current_user] ->
-        conn
+        put_current_user(conn, user)
 
       user = user_id && repo.get(Rumbl.User, user_id) ->
-        assign(conn, :current_user, user)
+        put_current_user(conn, user)
 
       true ->
         assign(conn, :current_user, nil)
@@ -28,7 +29,7 @@ defmodule Rumbl.Auth do
 
   def login(conn, user) do
     conn
-    |> assign(:current_user, user)
+    |> put_current_user(user)
     |> put_session(:user_id, user.id)
     |> configure_session(renew: true)
   end
@@ -63,5 +64,15 @@ defmodule Rumbl.Auth do
       |> redirect(to: Helpers.page_path(conn, :index))
       |> halt()
     end
+  end
+
+  # Private
+
+  defp put_current_user(conn, user) do
+    token = Phoenix.Token.sign(conn, "user socket", user.id)
+
+    conn
+    |> assign(:current_user, user)
+    |> assign(:user_token, token)
   end
 end
