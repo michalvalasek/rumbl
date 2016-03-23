00d23482e6e6888fdc18ccd5c8db3afc8528f134
Michal Valasek <michal.valasek@gmail.com>
2016-03-23 16:44:05 +0100

    Only load not-seen annotations from server when a user disconnects from channel and then reconnects.

--- web/static/js/video.js
+++ web/static/js/video.js
@@ -40,11 +40,14 @@ let Video = {
     })
 
     vidChannel.on("new_annotation", (resp) => {
+      vidChannel.params.last_seen_id = resp.id
       this.renderAnnotation(msgContainer, resp)
     })
 
     vidChannel.join()
       .receive("ok", resp => {
+        let ids = resp.annotations.map(ann => ann.id)
+        vidChannel.params.last_seen_id = Math.max(...ids)
         this.scheduleMessages(msgContainer, resp.annotations)
       })
       .receive("error", reason => console.log("joined failed", reason))
